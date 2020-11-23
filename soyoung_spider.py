20ecf6c750aeda6f40c25c47b0d3edaaa53c9b9b
steven <steven@nenyah.com>
2020-11-23 09:27:43 +0800

    refactor(soyoung_spider): refactor

--- soyoung_spider.py
+++ soyoung_spider.py
@@ -6,6 +6,7 @@ import datetime
 import logging
 import os
 import itertools
+from typing import List, Any
 
 import requests
 from requests import Response
@@ -16,7 +17,7 @@ logging.basicConfig(level=logging.INFO,
 log = logging.info
 
 
-def clean_text(text):
+def clean_text(text: str):
     return text.replace('\n', '').replace(' ', '')
 
 
@@ -24,37 +25,45 @@ class SoyoungSpider:
     """新氧爬虫
     :param keyword：关键词
     """
+    item: List[Any]
     root = 'http://www.soyoung.com/searchNew/'
     product_url = root + r'product?cityId=1&page_size=100&_json=1&sort=0'
     hospital_url = root + r'hospital?cityId=1&page_size=100&_json=1&sort=0'
 
     def __init__(self, keyword: str):
-        self.keyword: str = keyword
-        self.product_url: str = SoyoungSpider.product_url + '&keyword={}&page={}'
-        self.hospital_url: str = SoyoungSpider.hospital_url + '&keyword={}&page={}'
-        self.page: int = 1
-        self.headers: dict = {
+        self.keyword = keyword
+        self.product_url = SoyoungSpider.product_url + '&keyword={}&page={}'
+        self.hospital_url = SoyoungSpider.hospital_url + '&keyword={}&page={}'
+        self.page = 1
+        self.headers = {
             'User-Agent':
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \
-                (KHTML, like Gecko) Chrome/69.0.3497.81 Safari/537.36',
+                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
+                ' Chrome/86.0.4240.198 Safari/537.36 Edg/86.0.622.69',
             'referer':
-            'https://www.soyoung.com/',
+                'https://www.soyoung.com/',
             'Cookie':
-            'PHPSESSID=89b5e6b3dc9357c1d0da860bbc6791f8; __usersign__=1589161223881482893; __postion__=a%3A4%3A%7Bs%3A6%3A%22cityId%22%3Bi%3A0%3Bs%3A8%3A%22cityName%22%3Bs%3A0%3A%22%22%3Bs%3A8%3A%22cityCode%22%3Bi%3A0%3Bs%3A3%3A%22jwd%22%3Bi%3A0%3B%7D; _ga=GA1.2.1489774716.1589161703; _gid=GA1.2.1783629045.1589161703; Hm_lvt_b366fbb5465f5a86e1cc2871552e1fdb=1589161703; _gat=1; Hm_lpvt_b366fbb5465f5a86e1cc2871552e1fdb=1589162742'
+                'uuid=C3DBFC8B-4820-2E95-FF13-58BE047FF871; PHPSESSID=9a8a4aea7ee54cd65e946ea75f643951;'
+                ' __usersign__=1605862959183347602; '
+                '__postion__=a%3A4%3A%7Bs%3A6%3A%22cityId%22%3Bi%3A0%3Bs%3A8%3A%22'
+                'cityName%22%3Bs%3A0%3A%22%22%3Bs%3A8%3A%22cityCode%22%3Bi%3A0%3Bs%3A3%3A%22jwd%22%3Bi%3A0%3B%7D;'
+                ' _ga=GA1.2.1860693928.1605862962; _gid=GA1.2.1950432254.1605862962; _gat=1; '
+                'Hm_lvt_b366fbb5465f5a86e1cc2871552e1fdb=1605862962; '
+                'Hm_lpvt_b366fbb5465f5a86e1cc2871552e1fdb=1605862962'
         }
-        self.item: list = []
-        self.hospitals: list = []
-        self.count: int = 1
+        self.item = []
+        self.hospitals = []
+        self.count = 1
 
     def get_base_info(self) -> None:
-        url: str = self.product_url.format(self.keyword, self.page)
+        url = self.product_url.format(self.keyword, self.page)
         try:
             r: Response = requests.get(url, headers=self.headers)
             hasmore = r.json()['responseData']['has_more']
             products = r.json()['responseData']['arr_product']
         except Exception:
             log(f"[x] err:{url} parse fail")
-        if len(products) > 0:
+
+        if len(products):
             for product in products:
                 info = {
                     'link': 'http://y.soyoung.com/cp' + product['pid'],
@@ -79,7 +88,8 @@ class SoyoungSpider:
             hasmore = r.json()['responseData']['has_more']
         except Exception:
             log(f"[x] err:{url} parse fail")
-        if len(hospitals) > 0:
+
+        if len(hospitals):
             for hospital in hospitals:
                 info = {
                     'hospital_id': hospital['hospital_id'],
@@ -103,7 +113,7 @@ class SoyoungSpider:
         log(f'[+] Total item: {len(self.item)}')
 
         file = datetime.datetime.today().strftime('%Y-%m-%d') + \
-            f'{self.keyword}新氧销售情况.csv'
+               f'{self.keyword}新氧销售情况.csv'
         path = os.path.join(save_path, file)
         log(f'[+] Start to save file to {path}')
         with open(path, "w+", newline='', encoding='utf-8') as csvfile:
